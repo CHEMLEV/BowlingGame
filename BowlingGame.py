ba950310d9b0f603aae04060b95560f7fea49deb
CHEMLEV <88120172+CHEMLEV@users.noreply.github.com>
2022-09-14 21:26:15 +1200

    Move 'score' function to the end of 'BowlingGame' class Move 'frameScore' function to the top of 'BowlingGame' class Change '+2' to '+1' in 'spareScore' function Change 'if' condition in 'score' function
    Add 'append' function to 'rollMany' function
    Change first letter of 'StrikeScore' attribute to small
    Change function name from 'stickeScore' to 'strikeScore
    Rewrite 'testGutterGame' using 'rollMany' function
    Add 'append' function where 'rolls' list is called

--- BowlingGame.py
+++ BowlingGame.py
@@ -9,12 +9,27 @@ class BowlingGame:
     def roll(self, pins):
         self.rolls.append(pins)
 
+    def frameScore(self, rollIndex):
+        return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
+
+    def isStrike(self, rollIndex):
+        return self.rolls[rollIndex] == 10
+
+    def isSpare(self, rollIndex):
+        return self.rolls[rollIndex] + self.rolls[rollIndex + 1] == 10
+
+    def strikeScore(self, rollIndex):
+        return 10 + self.rolls[rollIndex + 1] + self.rolls[rollIndex + 2]
+
+    def spareScore(self, rollIndex):
+        return 10 + self.rolls[rollIndex + 1]
+
     def score(self):
         result = 0
         rollIndex = 0
         for frameIndex in range(10):
-            if frameIndex in range(10):
-                result += self.StrikeScore(rollIndex)
+            if self.isStrike(rollIndex):
+                result += self.strikeScore(rollIndex)
                 rollIndex += 1
             elif self.isSpare(rollIndex):
                 result += self.spareScore(rollIndex)
@@ -23,60 +38,50 @@ class BowlingGame:
                 result += self.frameScore(rollIndex)
             rollIndex += 2
             return result
-
-    def isStrike(self, rollIndex):
-        return self.rolls[rollIndex] == 10
-
-    def isSpare(self, rollIndex):
-        return self.rolls[rollIndex] + self.rolls[rollIndex + 1] == 10
-
-    def stickeScore(self, rollIndex):
-        return 10 + self.rolls[rollIndex + 1] + self.rolls[rollIndex + 2]
-
-    def spareScore(self, rollIndex):
-        return 10 + self.rolls[rollIndex + 2]
-
-    def frameScore(self, rollIndex):
-        return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
 class TestBowlingGame(unittest.TestCase):
 
     def setUp(self):
         self.game = BowlingGame()
 
     def testGutterGame(self):
-        for i in range(0, 20):
-            self.game.rolls(0)
+        self.rollMany(0,20)
+        print(self.game.score())
         assert self.game.score() == 0
 
     def testAllOnes(self):
         self.rollMany(1, 20)
+        print(self.game.score())
         assert self.game.score() == 20
 
     def testOneSpare(self):
-        self.game.rolls(5)
-        self.game.rolls(5)
-        self.game.rolls(3)
+        self.game.rolls.append(5)
+        self.game.rolls.append(5)
+        self.game.rolls.append(3)
         self.rollMany(0, 17)
+        print(self.game.score())
         assert self.game.score() == 16
 
     def testOneStrike(self):
-        self.game.rolls(10)
-        self.game.rolls(4)
-        self.game.rolls(3)
+        self.game.rolls.append(10)
+        self.game.rolls.append(4)
+        self.game.rolls.append(3)
         self.rollMany(0, 16)
+        print(self.game.score())
         assert self.game.score() == 24
 
     def testPerfectGame(self):
         self.rollMany(10, 12)
+        print(self.game.score())
         assert self.game.score() == 300
 
     def testOneSpare(self):
         self.rollMany(5, 21)
+        print(self.game.score())
         assert self.game.score() == 150
 
     def rollMany(self, pins, rolls):
         for i in range(rolls):
-            self.game.rolls(pins)
+            self.game.rolls.append(pins)
 
 
 # File 2 (BowlingGame.py)
